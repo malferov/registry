510a284f14c63ddceee03a7e1cadbe2a1e749e53
Maxim Alferov <malferov44@gmail.com>
2020-12-27 20:47:00 +0100

    fix kube config

--- manifest/kubeconfig.sh
+++ manifest/kubeconfig.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 kubectl config set-cluster kube --server=https://$K8S_SERVER:6443
-kubectl config set clusters.kube.certificate-authority-data $K8S_CAD
+kubectl config set-cluster kube --embed-certs --certificate-authority <(echo $K8S_CAD)
 kubectl config set-context ci@kube --cluster=kube --user=ci --namespace=$app
 kubectl config use-context ci@kube
-kubectl config set users.ci.client-certificate-data $K8S_CSD
-kubectl config set users.ci.client-key-data $K8S_CKD
+kubectl config set-credentials ci --embed-certs --client-certificate <(echo $K8S_CSD)
+kubectl config set-credentials ci --embed-certs --client-key <(echo $K8S_CKD)
