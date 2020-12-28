6a8fd4b29450e37a62ac67489402fab6a299d08f
Maxim Alferov <malferov44@gmail.com>
2020-12-28 23:43:26 +0100

    request validation

--- src/registry.go
+++ src/registry.go
@@ -9,6 +9,22 @@ import (
 	"regexp"
 )
 
+type Profile struct {
+	Accounts []string `json:"accounts" binding:"required"`
+	Address  string   `json:"address" binding:"required"`
+	Links    []string `json:"links" binding:"required"`
+	Logo     string   `json:"logo" binding:"required"`
+	Name     string   `json:"name" binding:"required"`
+}
+
+type LogLevel string
+
+const (
+	Info  = LogLevel("Info")
+	Error = LogLevel("Error")
+	Fatal = LogLevel("Fatal")
+)
+
 var (
 	sha     = "unknown"
 	version = "dev"
@@ -57,7 +73,7 @@ func statusOk(c *gin.Context) {
 
 func getCompany(c *gin.Context) {
 	company := c.Param("company")
-	glog.Info("getCompany: " + company)
+	log(Info, traceId(c), "Company: "+company, nil)
 
 	var alpha = regexp.MustCompile(`^[[:alpha:]]+$`).MatchString
 	if !alpha(company) {
@@ -77,21 +93,92 @@ func getCompany(c *gin.Context) {
 			"logo":   "iVBORw0KGgoAAAANSUhEU...AASUVORK5CYII=",
 		})
 	}
-	glog.Infof("getCompany: %d", http.StatusOK)
 }
 
 func putCompany(c *gin.Context) {
+	var profile Profile
 	company := c.Param("company")
-	glog.Info("getCompany: " + company)
+	log(Info, traceId(c), "Company: "+company, nil)
 
-	var alpha = regexp.MustCompile(`^[[:alpha:]]+$`).MatchString
-	if !alpha(company) {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "company name can contain only letters"})
-	} else {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"alias":      company,
-			"similarity": 64,
-			"error":      "logo similarity has exceeded threshold",
+	if genericValidation(c) {
+		var alpha = regexp.MustCompile(`^[[:alpha:]]+$`).MatchString
+		if !alpha(company) {
+			var msg = "company name can contain only letters"
+			log(Info, traceId(c), msg, nil)
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status": "validation",
+				"error":  msg,
+			})
+		} else {
+			//c.Header("Content-Type", "application/json") // BindJSON set text/plain if error
+			err := c.BindJSON(&profile)
+			if err != nil {
+				log(Info, traceId(c), "Invalid payload", err)
+				c.JSON(http.StatusBadRequest, gin.H{
+					"trace_id": *traceId(c),
+					"status":   "validation",
+					"error":    err.Error(),
+				})
+			} else {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"alias":      company,
+					"similarity": 64,
+					"error":      "logo similarity has exceeded threshold",
+				})
+			}
+		}
+	}
+}
+
+func genericValidation(c *gin.Context) bool {
+	var cType string
+
+	if c.Request.Header["Content-Type"] != nil {
+		cType = c.Request.Header["Content-Type"][0]
+	}
+
+	if cType != "application/json" {
+		var msg = "Content type '" + cType + "' is not supported"
+		log(Info, traceId(c), msg, nil)
+		c.JSON(http.StatusUnsupportedMediaType, gin.H{
+			"status": "validation",
+			"error":  msg,
 		})
+		return false
+	}
+	return true
+}
+
+func traceId(c *gin.Context) *string {
+	var h = c.Request.Header["X-Request-Id"]
+	var id string
+	if h != nil {
+		id = h[0]
+	} else {
+		id = "n/a"
+	}
+	return &id
+}
+
+func log(level LogLevel, traceId *string, message string, logErr error) {
+
+	if logErr != nil {
+		switch level {
+		case Info:
+			glog.Infof("%s: %v", message, logErr)
+		case Error:
+			glog.Errorf("%s: %v", message, logErr)
+		case Fatal:
+			glog.Fatalf("%s: %v", message, logErr)
+		}
+	} else {
+		switch level {
+		case Info:
+			glog.Infof("%s", message)
+		case Error:
+			glog.Errorf("%s", message)
+		case Fatal:
+			glog.Fatalf("%s", message)
+		}
 	}
 }
